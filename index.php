f0014209e62f3e45630d6cb3efbc23742e8ba14a
tombh <tom@tombh.co.uk>
2011-09-28 16:40:50 +0100

    initial commit

---
+++ index.php
@@ -0,0 +1,23 @@
+<html>
+	<head>
+		<link href='http://fonts.googleapis.com/css?family=Ubuntu:400,500,700' rel='stylesheet' type='text/css'> 
+		<style type="text/css">
+			body{
+				font-family: 'Ubuntu';
+				width: 1000px;
+				margin: auto;
+				color: #666;
+			}
+			li{
+				list-style-type: none;
+			}
+		</style>
+	</head>
+
+	<body>
+		<h1>The Great Reddit Comment Mine of September 2011</h1>
+		<p><strong>It all started <a href="http://www.reddit.com/r/AskReddit/comments/ks4da/we_had_to_temporarily_block_the_what/">here</a></strong></p>
+		<?php include('drill.html') ?>
+	</body>
+
+</html>
